54eaff00527872dbfb568110b3369e3afd9cd2cb
Dominik Hautau <dominikhautau@gmail.com>
2022-09-11 16:45:17 +0200

    setup project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # use-wasd
+
+a super easy hook to check which keys are currently pressed on your keyboard
---
+++ use-wasd.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+
+export default function useWASD(initialValue) {
+  const [keyboard, setKeyboard] = useState(
+    initialValue
+      ? initialValue
+      : {
+          w: false,
+          a: false,
+          s: false,
+          d: false,
+        }
+  );
+
+  useEffect(() => {
+    function handleKeyUp(event) {
+      handleKey(event, false);
+    }
+
+    function handleKeyDown(event) {
+      handleKey(event, true);
+    }
+
+    function handleKey(event, bool) {
+      const pressedKey = event.key.toLowerCase();
+      if (event.keyCode === 32) {
+        setKeyboard({ ...keyboard, space: bool });
+      } else {
+        setKeyboard({ ...keyboard, [pressedKey]: bool });
+      }
+    }
+
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [keyboard]);
+
+  return keyboard;
+}
