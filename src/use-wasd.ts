c861bc9555d464dbe5a79fddf891a29c2ba4d738
Dominik Hautau <dominikhautau@gmail.com>
2022-10-10 10:37:31 +0200

    added bundling and typing

--- src/use-wasd.ts
+++ src/use-wasd.ts
@@ -1,24 +1,24 @@
 import { useState, useEffect } from "react";
 
-export default function useWASD(initialValue = {}) {
+export default function useWASD(initialValue: Record<string, boolean> = {}) {
   const [keyboard, setKeyboard] = useState(initialValue);
 
   useEffect(() => {
-    function handleKeyUp(event) {
+    function handleKeyUp(event: KeyboardEvent) {
       handleKey(event.key, false);
     }
 
-    function handleKeyDown(event) {
+    function handleKeyDown(event: KeyboardEvent) {
       handleKey(event.key, true);
     }
 
-    function handleKey(key, bool) {
+    function handleKey(key: string, bool: boolean) {
       // Key might be "" in case of the space bar being pressed
       const key_ = key.toLowerCase().trim() || "space";
 
       setKeyboard((previousState) => ({
         ...previousState,
-        [key_]: bool
+        [key_]: bool,
       }));
     }
 
